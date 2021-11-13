c25c2593193efe6877d58aa57cc4dc75c3336791
w0rstR <sat2151@gmail.com>
2021-11-13 15:16:00 +0200

    Finished all tasks in lesson_1

--- lesson_1/classwork/index.js
+++ lesson_1/classwork/index.js
@@ -1,10 +1,96 @@
 // 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+
+// const arr=[]
+//
+// for (let i=0;i<10;i++){
+//     let num = Math.floor(Math.random()*10)
+//     arr.push(num)
+// }
+// console.log(arr)
+///////////////////////////////////////////////
+// let result=0;
+// for (let i=0; i<arr.length;i++){
+//     result+=arr[i]
+// }
+// console.log(result)
+//////////////////////////////////////////////
+
+/////////////////////////////
+// let result = 0;
+// arr.forEach(item=>{
+//     result+=item
+// })
+// console.log(result)
+/////////////////////////////////
+
+///////////////////////////////////////////////////////////
+// const result = arr.reduce((sum,currentValue)=>{
+//     return sum+=currentValue
+// })
+// console.log(result)
+//////////////////////////////////////////////////////////
+
+
+
+
+
 // 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+const first_book = {
+    name:'Apple',
+    count_of_page:500,
+    genre:'Fantasy',
+}
+
 // 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+const second_book={
+   name:'Clean Code',
+    genre:'programing',
+    author:'Robert C. Martin'
+}
 // 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+
+const third_book={
+    name:'Fantasy ',
+    count_of_page: 800,
+    genre:'Fantasy ',
+    author: 'Joanne Rowling'
+}
+
+const array_of_books=[first_book,second_book,third_book]
+
+for(let book of array_of_books){
+    console.log(book)
+}
+
 // 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
 // Значення площі зберігати в змінній s.
+function culc_area_of_rectengle(height,width){
+    const s=height*width;
+    console.log('Площа прямокутника:',s);
+    return s;
+}
+area_of_rectengle = culc_area_of_rectengle(23,10)
+
 // 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
 // результат помістіть у змінну v.
+
+function count_of_cylinder_capacity(heightC,dC){
+    const PI=3.14;
+    const v = PI * Math.pow(dC/2,2) / heightC;
+    console.log(`Обєм циліндра заввишки ${heightC}м та дыаметром основи ${dC}м дорівнює: ${v}`)
+    return v;
+}
+count_of_cylinder_capacity(10,4)
+
 // 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
 // Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+
+function count_of_hypotenuse(n,m){
+    const count = Math.sqrt(Math.pow(n,2)+Math.pow(m,2))
+    console.log(`Гіпотенуза прямокутного трикутника зі стронами ${n} та ${m} дорівнює: ${count}`)
+    return count
+}
+
+count_of_hypotenuse(3,4)
--- lesson_1/homework/script.js
+++ lesson_1/homework/script.js
@@ -1,153 +1,107 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
-let x = 'hello';
-let y = 'owu';
-let z = 'com';
-let j = 'ua';
-let g = 1;
-let t = 10;
-let q = -999;
-let w = 123;
-let k = 3.14;
-let e = 2.7;
-let u = 16;
-let p = true;
-let h = false;
+// let x = 'hello';
+// let y = 'owu';
+// let z = 'com';
+// let j = 'ua';
+// let g = 1;
+// let t = 10;
+// let q = -999;
+// let w = 123;
+// let k = 3.14;
+// let e = 2.7;
+// let u = 16;
+// let p = true;
+// let h = false;
 
 
 
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
-// console.log(x)
-// console.log(y)
-// console.log(z)
-// console.log(j)
-// console.log(g)
-// console.log(y)
-// console.log(q)
-// console.log(w)
-// console.log(k)
-// console.log(e)
-// console.log(u)
-// console.log(p)
-// console.log(h)
-//
-// alert(x)
-// alert(y)
-// alert(z)
-// alert(j)
-// alert(g)
-// alert(y)
-// alert(q)
-// alert(w)
-// alert(k)
-// alert(e)
-// alert(u)
-// alert(p)
-// alert(h)
-//
-// document.write(x)
-// document.write(y)
-// document.write(z)
-// document.write(j)
-// document.write(g)
-// document.write(y)
-// document.write(q)
-// document.write(w)
-// document.write(k)
-// document.write(e)
-// document.write(u)
-// document.write(p)
-// document.write(h)
 
+// let allVariables=`
+//     ${x}
+//     ${y}
+//     ${z}
+//     ${j}
+//     ${g}
+//     ${t}
+//     ${q}
+//     ${w}
+//     ${k}
+//     ${e}
+//     ${u}
+//     ${p}
+//     ${h}
+// `
+// console.log(x,y,z,j,g,t,q,w,k,e,u,p,h)
+// document.write(allVariables)
+// alert(allVariables)
 
 // - Переприсвоїти кожній змінній з завдання значення на довільне.
 
-x = 'owu';
-y = 'com';
-z = 'ua';
-j = 'HELLO';
-g = 277713;
-t = 103122;
-q = 9993122;
-w = 131231231223;
-k = 333.14444;
-e = 1112.7444;
-u = 1214124126;
-p = false
-h = false
+// x = 'owu';
+// y = 'com';
+// z = 'ua';
+// j = 'HELLO';
+// g = 277713;
+// t = 103122;
+// q = 9993122;
+// w = 131231231223;
+// k = 333.14444;
+// e = 1112.7444;
+// u = 1214124126;
+// p = false
+// h = false
 
 
-console.log(x)
-console.log(y)
-console.log(z)
-console.log(j)
-console.log(g)
-console.log(y)
-console.log(q)
-console.log(w)
-console.log(k)
-console.log(e)
-console.log(u)
-console.log(p)
-console.log(h)
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
-
-
-console.log(x)
-console.log(y)
-console.log(z)
-console.log(j)
-console.log(g)
-console.log(y)
-console.log(q)
-console.log(w)
-console.log(k)
-console.log(e)
-console.log(u)
-console.log(p)
-console.log(h)
-
-
-// alert(x)
-// alert(y)
-// alert(z)
-// alert(j)
-// alert(g)
-// alert(y)
-// alert(q)
-// alert(w)
-// alert(k)
-// alert(e)
-// alert(u)
-// alert(p)
-// alert(h)
 //
-// document.write(x)
-// document.write(y)
-// document.write(z)
-// document.write(j)
-// document.write(g)
-// document.write(y)
-// document.write(q)
-// document.write(w)
-// document.write(k)
-// document.write(e)
-// document.write(u)
-// document.write(p)
-// document.write(h)
+// let secondAllVariables=`
+//     ${x}
+//     ${y}
+//     ${z}
+//     ${j}
+//     ${g}
+//     ${t}
+//     ${q}
+//     ${w}
+//     ${k}
+//     ${e}
+//     ${u}
+//     ${p}
+//     ${h}
+// `
+// console.log(x,y,z,j,g,t,q,w,k,e,u,p,h)
+// document.write(secondAllVariables)
+// alert(secondAllVariables)
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
 
-const firstName = 'Maksym';
-const lastName = 'Saturchak';
-const middleName = 'Oleksandrovych'
-
-const person = firstName + ' ' +lastName + ' ' + middleName
-console.log(person)
+// const firstName = 'Maksym';
+// const lastName = 'Saturchak';
+// const middleName = 'Oleksandrovych'
+//
+// const person = firstName + ' ' +lastName + ' ' + middleName
+// console.log(person)
 
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+
+// const firstName = prompt('Введіть ваше імя: ')
+// const middleName = prompt('Введіть ваше імя по-батькові: ')
+// const ages = prompt('Скільки вам років?')
+//
+// console.log(`Вітаю ${firstName} ${middleName}. Тобі ${ages} років`)
+
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 //     let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+
 // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
 //     В однакових виразах не використовувати однакові оператори!!!
 //     5 ? 6 -> true
@@ -161,7 +115,21 @@ console.log(person)
 // 10 ? 10 -> false
 // 123 ? '123' -> false
 // 123 ? '123' -> true
-//
+
+// const first_num = 5 > 6
+// const second_num =  5 === 6
+// const third_num =  5 >= 6
+// const fourth_num =  10 == 10
+// const fifth_num =  10 <= 10
+// const sixth_num = 10 !== 10
+// const seventh_num = 10 < 10
+// const eight_num = 10 > 10
+// const nineth_num = 123 === '123'
+// const tenth_num = 123 == '123'
+// console.log(first_num,second_num,third_num,fourth_num,fifth_num,sixth_num,seventh_num,eight_num,nineth_num,tenth_num)
+
+
+
 // - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
 //     let str = "20";
 // let a = 5;
@@ -169,3 +137,6 @@ console.log(person)
 // document.write(str - a + "<br/>");
 // document.write(str * "2" + "<br/>");
 // document.write(str / 2 + "<br/>");
+//
+// В першому випадку у нас строка 205 тому, що для строк є оператор +, змінну a конвертне в строку і складе дві строки.
+// у всіх інших випадка буде number тому, що для строк немає оператора -,*,/ . str конвертне в число і виконає ці операції.Тобто в 3 останніх результат буде number
