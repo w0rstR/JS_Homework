d8eaf63404afabd36fd58876ab5ac5b63c5a3a62
w0rstR <sat2151@gmail.com>
2021-11-13 09:27:49 +0200

    Added tasks for homework/classwork

---
+++ lesson_1/homework/script.js
@@ -0,0 +1,171 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let x = 'hello';
+let y = 'owu';
+let z = 'com';
+let j = 'ua';
+let g = 1;
+let t = 10;
+let q = -999;
+let w = 123;
+let k = 3.14;
+let e = 2.7;
+let u = 16;
+let p = true;
+let h = false;
+
+
+
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// console.log(j)
+// console.log(g)
+// console.log(y)
+// console.log(q)
+// console.log(w)
+// console.log(k)
+// console.log(e)
+// console.log(u)
+// console.log(p)
+// console.log(h)
+//
+// alert(x)
+// alert(y)
+// alert(z)
+// alert(j)
+// alert(g)
+// alert(y)
+// alert(q)
+// alert(w)
+// alert(k)
+// alert(e)
+// alert(u)
+// alert(p)
+// alert(h)
+//
+// document.write(x)
+// document.write(y)
+// document.write(z)
+// document.write(j)
+// document.write(g)
+// document.write(y)
+// document.write(q)
+// document.write(w)
+// document.write(k)
+// document.write(e)
+// document.write(u)
+// document.write(p)
+// document.write(h)
+
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+
+x = 'owu';
+y = 'com';
+z = 'ua';
+j = 'HELLO';
+g = 277713;
+t = 103122;
+q = 9993122;
+w = 131231231223;
+k = 333.14444;
+e = 1112.7444;
+u = 1214124126;
+p = false
+h = false
+
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(j)
+console.log(g)
+console.log(y)
+console.log(q)
+console.log(w)
+console.log(k)
+console.log(e)
+console.log(u)
+console.log(p)
+console.log(h)
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(j)
+console.log(g)
+console.log(y)
+console.log(q)
+console.log(w)
+console.log(k)
+console.log(e)
+console.log(u)
+console.log(p)
+console.log(h)
+
+
+// alert(x)
+// alert(y)
+// alert(z)
+// alert(j)
+// alert(g)
+// alert(y)
+// alert(q)
+// alert(w)
+// alert(k)
+// alert(e)
+// alert(u)
+// alert(p)
+// alert(h)
+//
+// document.write(x)
+// document.write(y)
+// document.write(z)
+// document.write(j)
+// document.write(g)
+// document.write(y)
+// document.write(q)
+// document.write(w)
+// document.write(k)
+// document.write(e)
+// document.write(u)
+// document.write(p)
+// document.write(h)
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+const firstName = 'Maksym';
+const lastName = 'Saturchak';
+const middleName = 'Oleksandrovych'
+
+const person = firstName + ' ' +lastName + ' ' + middleName
+console.log(person)
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
